63d2fb41780b408d8368e2310065cd8e4da18df6
Muhammad R <73916528+MoRanderee@users.noreply.github.com>
2024-04-22 23:45:49 +0200

    initial upload

---
+++ app.js
@@ -0,0 +1,47 @@
+// effect for header left
+ScrollReveal().reveal('.header-left',{
+    duration: 1500,
+    origin: "left",
+    distance:"200px",
+    easing:"ease-in-out"
+})
+
+// effect for header right
+ScrollReveal().reveal('.header-right',{
+    duration: 2000,
+    scale: 0.85
+})
+
+// effect for title
+ScrollReveal().reveal('.top-title',{
+    duration: 2000,
+    move: 0
+})
+
+// effect for about section
+ScrollReveal().reveal('.about-card',{
+    duration: 1500,
+    origin: "bottom",
+    distance:"100px",
+    easing:"ease-in-out"
+})
+
+// effect for projects section
+ScrollReveal().reveal('.projects-card',{
+    duration: 2000,
+    scale: 0.85
+})
+
+// effect for expertise section
+ScrollReveal().reveal('.expertise-card',{
+    duration: 2000,
+    scale: 0.85
+})
+
+// effect for contact section
+ScrollReveal().reveal('.contact-wrapper',{
+    duration: 200,
+    origin: "bottom",
+    distance:"100px",
+    easing:"ease-in-out"
+})
